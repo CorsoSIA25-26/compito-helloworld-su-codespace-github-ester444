a22e7e5127379882c52fced1bf8094924f94eeb8
ester444 <ester.farruku@iisleinaudi.edu.it>
2025-10-20 23:48:27 +0200

    Replace hello world with club entry program

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 int main()
-{
-    cout << "Hello world!" << endl;
-}
+{const int ingressoMin = 15;
+ const int ingressoMagg = 18;
+ int numMin,numMagg,totRagazzi,incassoMin,incassoMagg;
+cout << "Quanti minorenni sono entrati nella discoteca ?" << endl;
+cin>>numMin;
+cout << "Quanti maggiorenni sono entrati nella discoteca ?" << endl;
+cin>>numMagg;
+totRagazzi=numMin+numMagg;
+cout << "Complessivamente sono entrati " << totRagazzi << " ragazzi in discoteca." << endl;
+incassoMin=numMin*ingressoMin;
+incassoMagg=numMagg*ingressoMagg;
+cout << "L'incasso per l'entrata dei minorenni e' : " << incassoMin << " euro" << endl;
+cout << "L'incasso per l'entrata dei maggiorenni e' : " << incassoMagg << " euro" << endl;
+
+ }
+
